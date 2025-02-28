0df4523f8db5f03017b7180efe42129a588bb735
Roney Moon <112367082+roneymoon@users.noreply.github.com>
2025-02-28 22:11:44 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connection_string = "mongodb+srv://roney:r0newhy123@node-express.iijuo.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&appName=node-express"
 
 const connectDB = (url) => {
-    return mongoose.connect(url, {
+    return mongoose.connect(connection_string, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
